1a39a0c8b9d4d17bb06757807e94c6564bce7360
Clément "Doom" Doumergue <clement.doumergue@epitech.eu>
2025-01-20 12:34:23 +0100

    feat: add `TakeWhile` / `Take` / `DropWhile` / `Drop` / `Chain` functions (#2)

--- itertools_test.go
+++ itertools_test.go
@@ -53,3 +53,73 @@ func TestItertools_Filter(t *testing.T) {
 	ss = itertools.Filter(Empty[int](), func(_ int) bool { return true })
 	assert.Equal(t, []int(nil), slices.Collect(ss))
 }
+
+func TestItertools_TakeWhile(t *testing.T) {
+	is := itertools.TakeWhile(IntRange(0, 5), func(i int) bool { return i < 3 })
+	assert.Equal(t, []int{0, 1, 2}, slices.Collect(is))
+
+	is = itertools.TakeWhile(IntRange(0, 5), func(i int) bool { return true })
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, slices.Collect(is))
+
+	is = itertools.TakeWhile(IntRange(0, 5), func(i int) bool { return false })
+	assert.Equal(t, []int(nil), slices.Collect(is))
+
+	ss := itertools.TakeWhile(Empty[string](), func(i string) bool { return true })
+	assert.Equal(t, []string(nil), slices.Collect(ss))
+}
+
+func TestItertools_Take(t *testing.T) {
+	is := itertools.Take(IntRange(0, 5), 3)
+	assert.Equal(t, []int{0, 1, 2}, slices.Collect(is))
+
+	is = itertools.Take(IntRange(0, 5), 10)
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, slices.Collect(is))
+
+	is = itertools.Take(IntRange(0, 5), 0)
+	assert.Equal(t, []int(nil), slices.Collect(is))
+
+	ss := itertools.Take(Empty[string](), 5)
+	assert.Equal(t, []string(nil), slices.Collect(ss))
+}
+
+func TestItertools_DropWhile(t *testing.T) {
+	is := itertools.DropWhile(IntRange(0, 5), func(i int) bool { return i < 3 })
+	assert.Equal(t, []int{3, 4}, slices.Collect(is))
+
+	is = itertools.DropWhile(IntRange(0, 5), func(i int) bool { return true })
+	assert.Equal(t, []int(nil), slices.Collect(is))
+
+	is = itertools.DropWhile(IntRange(0, 5), func(i int) bool { return false })
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, slices.Collect(is))
+
+	ss := itertools.DropWhile(Empty[string](), func(i string) bool { return false })
+	assert.Equal(t, []string(nil), slices.Collect(ss))
+}
+
+func TestItertools_Drop(t *testing.T) {
+	is := itertools.Drop(IntRange(0, 5), 3)
+	assert.Equal(t, []int{3, 4}, slices.Collect(is))
+
+	is = itertools.Drop(IntRange(0, 5), 10)
+	assert.Equal(t, []int(nil), slices.Collect(is))
+
+	is = itertools.Drop(IntRange(0, 5), 0)
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, slices.Collect(is))
+
+	ss := itertools.Drop(Empty[string](), 0)
+	assert.Equal(t, []string(nil), slices.Collect(ss))
+}
+
+func TestItertools_Chain(t *testing.T) {
+	is := itertools.Chain(Empty[int](), Empty[int]())
+	assert.Equal(t, []int(nil), slices.Collect(is))
+
+	is = itertools.Chain(Empty[int](), IntRange(0, 5))
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, slices.Collect(is))
+
+	is = itertools.Chain(IntRange(0, 5), Empty[int]())
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, slices.Collect(is))
+
+	is = itertools.Chain(IntRange(0, 5), IntRange(5, 10))
+	assert.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, slices.Collect(is))
+}
